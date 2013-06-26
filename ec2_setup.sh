c7242df9136d23cc94706f88a16b76dcac081516
shenan1984 <shenan1984@gmail.com>
2013-06-26 08:33:49 -0400

    setup script

---
+++ ec2_setup.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo apt-get install vim
+sudo apt-get install git
+sudo apt-get install nodejs
+sudo apt-get install openssh-server openssh-client
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku login
